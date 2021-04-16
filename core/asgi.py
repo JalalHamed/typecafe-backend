514ae6388de86ff849540de41e656779bc9d9b4c
Jalal Hamed <iamjalalhamed@gmail.com>
2021-04-16 17:34:52 +0430

    updates settings.py and asgi.py for channels use case

--- core/asgi.py
+++ core/asgi.py
@@ -1,7 +1,7 @@
 import os
 
-from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
