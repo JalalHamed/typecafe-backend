e3d953658280c1e3c2e8b3ea8592333461282116
Jalal Hamed <iamjalalhamed@gmail.com>
2021-04-24 23:55:11 +0430

    first websocket data got through 2 separate loads of the website. (CONGRATS)

--- project/consumers.py
+++ project/consumers.py
@@ -1,17 +1,32 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 class TcConsumer(WebsocketConsumer):
     def connect(self):
+        async_to_sync(self.channel_layer.group_add)(
+            'tc',
+            self.channel_name
+        )
         self.accept()
         
     def receive(self, text_data):
         data = json.loads(text_data)
-        if data['status'] == 'newProject':
+        status = data['status']
+        if status == 'newProject':
             self.send(text_data=json.dumps({
-                'project': data
+                'data': data
             }))
+        if status == 'time':
+            async_to_sync(self.channel_layer.group_send)('tc', {
+                'type': 'time',
+                'data': json.dumps(data)
+            })
         
+    def time(self, event):
+        self.send(text_data=json.dumps({
+            'data': event
+        }))
 
-    def disconnect(self):
-        pass
+    def disconnect(self, close_code):
+        print('disconnected', close_code)
